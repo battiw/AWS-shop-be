c7c37c73da80123355a1271855cc4947d12bd25a
Wit <battiwgood@gmail.com>
2022-06-23 18:26:50 +0300

    feat: add functions returns error 400 status code if product data is invalid

--- travel-servis/handler/postProducts.js
+++ travel-servis/handler/postProducts.js
@@ -23,9 +23,28 @@ export const postProducts = async (event) => {
 
   try {
     const bodyPost = await JSON.parse(event.body);
+
     const { title, description, price, count } = bodyPost;
 
+    if (typeof title == "number")
+      throw "Invalid input type. TITLE must be a string, not a number";
+    if (title == null) throw "Invalid input type.";
+    if (title === "") throw "Invalid input. Enter data";
+    if (typeof description == "number")
+      throw "Invalid input type. DESCRIPTION must be a string, not a number";
+    if (description == null) throw "Invalid input type.";
+    if (description === "") throw "Invalid input. Enter data";
+    if (typeof price == "string")
+      throw "Invalid input type. PRICE must be a number, not a string";
+    if (price == null) throw "Invalid input type.";
+    if (price === 0) throw "Invalid input. Enter data";
+    if (typeof count == "string")
+      throw "Invalid input type. COUNT must be a number, not a string";
+    if (count == null) throw "Invalid input type.";
+    if (count === 0) throw "Invalid input. Enter data";
+
     await client.query("BEGIN");
+
     const dnlResultProducts = await client.query(`
     insert into products ( title, description, price) values
     ('${title}', '${description}', '${price}') returning id`);
@@ -47,21 +66,40 @@ export const postProducts = async (event) => {
         "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
       },
       body: JSON.stringify(bodyPost),
+
       isBase64Encoded: false,
     };
   } catch (err) {
     await client.query("ROLLBACK");
+
     console.error("ErRoR", err);
-    return {
-      statusCode: 500,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Credentials": true,
-      },
-      body: JSON.stringify({
-        message: "Unhandled error",
-      }),
-    };
+
+    if (err) {
+      return {
+        statusCode: 401,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+          "Content-Type": "application/JSON",
+          "Access-Control-Allow-Headers": "Content-Type",
+          "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+        },
+        body: err,
+
+        isBase64Encoded: false,
+      };
+    } else {
+      return {
+        statusCode: 500,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+        },
+        body: JSON.stringify({
+          message: "Unhandled error",
+        }),
+      };
+    }
   } finally {
     client.end();
   }
